defe7978f082ef15d7633dba221a4b1cc826f920
caterpilllar <123208987+caterpilllar@users.noreply.github.com>
2024-04-21 23:56:50 -0400

    done

--- .eleventy.js
+++ .eleventy.js
@@ -9,40 +9,54 @@ module.exports = function(eleventyConfig) {
     return `<div class="user"> <div class="user_name"> ${name}</div> <div class="user_twitter"> @${twitterUsername} </div> </div>`
   });
 
+  eleventyConfig.addShortcode("nav", function(input){
+    return `
+    <div class='mx-5'>
+      <a class='btn btn-outline-secondary col-2'>Popular</a>
+      <a class='btn btn-secondary col'>Discover</a>
+      <a>Animation</a>
+      <a>Branding</a>
+      <a>Illustration</a>
+      <a>Mobile</a>
+      <a>Print</a>
+      <a class='btn btn-outline-secondary col-2'>Filters</a>
+    </div>
+    `
+  });
+
 
   eleventyConfig.addShortcode("post", function(image, logo, name, group, likes, views){
     return`
-    <section class="container">
+    <section class="container m-5">
     <img src="${image}" class='rounded' style='width:100%'>
 
-
       <section class='row'>
-       <section class='col-1'>
-          <img src="${logo}" style='width:80%; '>
+       <section class='col-2'>
+          <img class='w-75' src="${logo}"; >
          </section>
 
-        <section class='col-1''>
-          <h1>${name}</h1>
+        <section class='col-2'>
+          <h1 class='w-75'>${name}</h1>
         </section>
 
-        <section class='col-1' style='margin:0px;'>
-          <img src="${group}">
+        <section class='col-2' '>
+          <img class='w-75' src="${group}">
         </section>
 
-        <section class='col-3'>
-          <img src="images/heart.png" style='width:80%;'>
-        </section>
-
-        <section class='col-1'>
-          <p>${likes}</p>
+        <section class='col-2'>
+          <img class='w-75' src="images/heart.png">
         </section>
 
         <section class='col-1'>
-          <img src="images/eye.png" style='width:80%'>
+          <p class='w-75'>${likes}</p>
+        </section>
+
+        <section class='col-2'>
+          <img class='w-75' src="images/eye.png">
         </section>
 
         <section class='col-1'>
-          <p>${views}</p>
+          <p class='w-75'>${views}</p>
         </section>
 
       </section>
